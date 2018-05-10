c6b829ce49fa08bf1c0294fc07ea263461f63b63
Cornelius <ces92@users.noreply.github.com>
2018-05-10 13:01:51 +0200

    remove git tag in template

--- bvdata/data/context_processors.py
+++ bvdata/data/context_processors.py
@@ -1,16 +1,14 @@
 from bvdata.data.models import GastroSubmit
-from .utils import commit_hash, tag_name
+from .utils import commit_hash
 
 
 def commit(request):
     id = commit_hash()
     id_short = commit_hash(short=True)
-    tag = tag_name()
 
     git = dict(
         id=id,
         id_short=id_short,
-        tag=tag,
         url=f'https://github.com/Berlin-Vegan/berlin-vegan-data/commits/master'
     )
 
