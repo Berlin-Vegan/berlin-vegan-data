48e5b82e64cab0f250763360491fb1e2f32bb476
Cornelius <ces92@users.noreply.github.com>
2019-11-01 22:47:25 +0100

    Adjust deploy

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#git checkout master
-#git pull
-
+git checkout master
+git pull
 docker-compose up -d --build
-
-#migration
-docker-compose exec django sh -c "pipenv run python manage.py migrate"
-
-#compilemessages
-docker-compose  exec django sh -c "pipenv run python manage.py compilemessages -l de"
-
-#collect static
-docker-compose  exec django sh -c "pipenv run python manage.py collectstatic --noinput"
