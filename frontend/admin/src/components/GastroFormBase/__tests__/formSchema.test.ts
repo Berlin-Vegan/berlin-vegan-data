62f6bc689c105d8dfbbf21e1fa4330ce2d513d4e
Cornelius <ces92@users.noreply.github.com>
2020-10-05 19:03:40 +0200

    Add validation for opening hours string

--- frontend/admin/src/components/GastroFormBase/__tests__/formSchema.test.ts
+++ frontend/admin/src/components/GastroFormBase/__tests__/formSchema.test.ts
@@ -1,8 +1,25 @@
 import GastroDetailValid from './fixtures/GastroDetailVaild.json';
-import gastroFormSchema from '../formSchema';
+import gastroFormSchema, { testTimeString } from '../formSchema';
 
 test('valid gastro object', () => {
   return gastroFormSchema
     .validate(GastroDetailValid)
     .then((result) => expect(result).toBeTruthy());
+});
+
+test('testTimeString true', () => {
+  expect(testTimeString('00:00:00')).toBeTruthy();
+  expect(testTimeString('01:01:01')).toBeTruthy();
+  expect(testTimeString('10:59:59')).toBeTruthy();
+  expect(testTimeString('11:59:59')).toBeTruthy();
+  expect(testTimeString('23:59:59')).toBeTruthy();
+});
+
+test('testTimeString false', () => {
+  expect(testTimeString('00:00')).toBeFalsy();
+  expect(testTimeString('01:01-01')).toBeFalsy();
+  expect(testTimeString('10:59:60')).toBeFalsy();
+  expect(testTimeString('11:60:59')).toBeFalsy();
+  expect(testTimeString('24:00:00')).toBeFalsy();
+  expect(testTimeString('24:10:10')).toBeFalsy();
 });
